aff7124c055738a496545cab79c5802ae252090f
testesdebots <78939617+testesdebots@users.noreply.github.com>
2021-02-17 10:38:38 -0300

    Add files via upload

---
+++ arco/help.js
@@ -0,0 +1,25 @@
+const help = () => {
+	return '> ```Comandos de fig``` <\n' +
+	'comando : *!sticker* ou *!stiker*\n' +
+	'desc : converte imagem/gif/video to sticker\n' +
+	'usage : imagem de resposta/gif/video, ou enviar imagem/gif/vídeo com legenda\n\n' + 
+	'command : *!toimg*\n' +
+	'desc : convert sticker to image\n' +
+	'usage : reply sticker\n\n' +
+	'command : *!tsticker* or *!tstiker*\n' +
+	'desc : convert text to sticker\n' +
+	'usage : *!tsticker text in here*\n\n' +
+	'> ```Meme Commands``` <\n' +
+	'command : *!meme*\n' +
+	'desc : random meme images [english]\n' +
+	'usage : just send the command\n\n' +
+	'command : *!memeindo*\n' +
+	'desc : random meme images [indo]\n' +
+	'usage : just send the command\n\n' +
+	'> ```Text To Speech Command``` <\n' +
+	'command : *!gtts*\n' +
+	'desc : convert text to speech/audio\n' +
+	'usage : *!gtts [cc] [text]*\nexample : *!gtts ja On2-chan*'
+}
+
+exports.help = help
